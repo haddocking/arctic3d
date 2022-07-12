49d562619eaee920bb9b4a40594f42c75d34babb
AldovdN <90687940+AldovdN@users.noreply.github.com>
2022-07-12 13:26:51 +0200

    implemented blast in blast.py

--- src/arctic3d/modules/blast.py
+++ src/arctic3d/modules/blast.py
@@ -1,3 +1,7 @@
+from Bio.Blast import NCBIWWW
+from defusedxml import lxml as ET
+
+
 def blast_seq(fasta_seq):
     """
     Blast sequence.
@@ -13,4 +17,18 @@ def blast_seq(fasta_seq):
         Uniprot ID.
 
     """
+    blast_res_handle = NCBIWWW.qblast("blastp", "nr", fasta_seq, hitlist_size=50)
+
+    with open("blast_res.xml", "w") as save_output:
+        blast_res = blast_res_handle.read()
+        save_output.write(blast_res)
+    
+    tree = ET.parse("blast_res.xml")
+    root = tree.getroot()
+
+    # root [BlastOutput_iterations] [Iteration] [Iteration_hits] [Hit #2] [Hit_accession]
+    # using second hit as the first is the input
+    # instead of Hit_accession, [1] for [Hit_id] can be used
+    accession_id = root[8][0][4][1][3]
+
     pass
