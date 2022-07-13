c6bdd748b4a5f64b610d1074548ee8ec56dc08ae
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2022-07-13 14:10:57 +0200

    add option to run blast locally

--- src/arctic3d/modules/blast.py
+++ src/arctic3d/modules/blast.py
@@ -1,18 +1,69 @@
 """Function to BLAST input sequence and return accession id."""
+import logging
 import os
+import shlex
+import shutil
+import subprocess
+from pathlib import Path
 
 from Bio.Blast import NCBIWWW
 from defusedxml import lxml as ET
 
+log = logging.getLogger("arctic3dlog")
 
-def blast_seq(fasta_seq):
+
+def run_blast(fasta_f, db=None):
     """
-    Blast sequence.
+    Run BLAST.
 
     Parameters
     ----------
-    fasta_seq : str
-        Fasta sequence.
+    local : bool
+        Run BLAST locally.
+
+    """
+    if db:
+        log.info(f"Running BLAST locally against {Path(db).name}")
+        accession_id = blast_local(fasta_f, db)
+    else:
+        log.info("Running BLAST remotely...")
+        accession_id = blast_remote(fasta_f)
+
+    return accession_id
+
+
+def get_blast_exec():
+    """
+    Get BLAST executable.
+
+    Returns
+    -------
+    blastp_exec : str
+        BLASTp executable.
+
+    """
+    if shutil.which("blastp"):
+        blastp_exec = "blastp"
+    else:
+        ncbi_blast_path = [
+            f
+            for f in Path(__file__).parent.parent.parent.glob("ncbi-blast*")
+            if f.is_dir()
+        ][0]
+        blastp_exec = Path(ncbi_blast_path, "bin/blastp")
+        if not blastp_exec.exists():
+            log.error("Could not find blastp executable")
+    return blastp_exec
+
+
+def blast_local(fasta_file, db):
+    """
+    Blast sequence against Uniprot locally.
+
+    Parameters
+    ----------
+    fasta_file : str
+        Fasta filename
 
     Returns
     -------
@@ -20,8 +71,38 @@ def blast_seq(fasta_seq):
         Uniprot ID.
 
     """
-    print("running BLAST")
-    blast_res_handle = NCBIWWW.qblast("blastp", "nr", fasta_seq, hitlist_size=50)
+    blastp_exec = get_blast_exec()
+    cmd = f"{blastp_exec} -query {fasta_file} -db {db} -outfmt 6"
+
+    p = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if p.returncode != 0:
+        log.error(p.stderr.decode())
+        raise Exception("BLAST failed")
+
+    out = p.stdout.decode("utf-8").split(os.linesep)
+    uniprot_id = out[0].split("\t")[1]
+    return uniprot_id
+
+
+def blast_remote(fasta_file):
+    """
+    Blast sequence.
+
+    Parameters
+    ----------
+    fasta_seq : str
+        Fasta filename.
+
+    Returns
+    -------
+    uniprot_id : str
+        Uniprot ID.
+
+    """
+    blast_res_handle = NCBIWWW.qblast(
+        "blastp", "swissprot", fasta_file, hitlist_size=50
+    )
 
     # temp file for storing results
     with open("blast_res.xml", "w") as save_output:
@@ -35,8 +116,5 @@ def blast_seq(fasta_seq):
     # using second hit as the first is the input
     # instead of Hit_accession, [1] for [Hit_id] can be used
     accession_id = root[8][0][4][1][3].text
-
-    # cleaning of temporary result file
-    os.remove("blast_res.xml")
 
     return accession_id
