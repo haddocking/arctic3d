9d142ff84d00d187b98edcc6acf1d1299ab6f8f4
Marco Giulini <54807167+mgiulini@users.noreply.github.com>
2022-08-29 10:17:46 +0200

    Exclude pdb (#53)
    * implemented out_pdb logic
    
    * added tests for parse_out_pdb
    
    * fixed linting
    
    * list to set
    
    Co-authored-by: “mgiulini” <“mrcgiulini@gmail.com”>

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -2,7 +2,11 @@ from pathlib import Path
 
 import pytest
 
-from arctic3d.modules.interface import parse_out_uniprot, read_interface_residues
+from arctic3d.modules.interface import (
+    parse_out_pdb,
+    parse_out_uniprot,
+    read_interface_residues,
+)
 
 from . import golden_data
 
@@ -36,3 +40,27 @@ def test_parse_out_uniprot():
         obs_list = parse_out_uniprot(string)
         observed_uniprot_strings.append(obs_list)
     assert expected_uniprot_strings == expected_uniprot_strings
+
+
+def test_parse_out_pdb():
+    """Test parse_out_pdb function."""
+    out_pdb_strings = [None, "1abc", "4B2C,4B1T,4B2A", "4B2C,4B1T,4B2A,4b2a"]
+    expected_pdb_sets = [
+        set([]),
+        set(["1abc"]),
+        set(["4b2a", "4b2c", "4b1t"]),
+        set(["4b2a", "4b2c", "4b1t"]),
+    ]
+    observed_pdb_sets = []
+    for string in out_pdb_strings:
+        obs_pdb_set = parse_out_pdb(string)
+        observed_pdb_sets.append(obs_pdb_set)
+    assert expected_pdb_sets == observed_pdb_sets
+
+
+def test_error_parse_out_pdb():
+    """Test correct exception of parse_out_pdb function."""
+    out_pdb_strings = ["1ab", "1abcd", "4B2C,4B1T,4B2+"]
+    for string in out_pdb_strings:
+        with pytest.raises(Exception):
+            parse_out_pdb(string)
