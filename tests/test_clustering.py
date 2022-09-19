acfb6a5a87a524ac3bfa88827d3d20f85a330d19
Marco Giulini <54807167+mgiulini@users.noreply.github.com>
2022-09-19 18:10:08 +0200

    refactored clustering (#87)
    * refactored clustering
    
    * removed return from write_residues

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -1,8 +1,29 @@
-import numpy as np
+import os
 
-from arctic3d.modules.clustering import (  # write_clusters,; write_residues,
+import numpy as np
+import pytest
+
+from arctic3d.modules.clustering import (
     cluster_similarity_matrix,
+    get_clustering_dict,
+    get_residue_dict,
+    write_clusters,
+    write_residues,
 )
+
+
+@pytest.fixture
+def reference_cl_dict():
+    """Reference dictionary of clustered interfaces."""
+    cl_dict = {1: ["int_1", "int_2"], 2: ["int_3"]}
+    return cl_dict
+
+
+@pytest.fixture
+def reference_res_dict():
+    """Reference dictionary of clustered interfaces."""
+    res_dict = {1: [1, 2, 3, 4, 5], 2: [27, 28, 29]}
+    return res_dict
 
 
 def test_cluster_similarity_matrix():
@@ -14,11 +35,48 @@ def test_cluster_similarity_matrix():
     assert (clusters == expected_clusters).all()
 
 
-def test_write_clusters():
-    # TODO: implement this
-    assert True
+def test_get_cl_dict():
+    """Test correct retrieval of cl_dict."""
+    clusters_list = [1, 1, 2, 3, 3, 4, 2]
+    ligands_list = ["int1", "int2", "p53", "00", "int47", "antibody", "dimer"]
+    expected_cl_dict = {
+        1: ["int1", "int2"],
+        2: ["p53", "dimer"],
+        3: ["00", "int47"],
+        4: ["antibody"],
+    }
+    observed_cl_dict = get_clustering_dict(clusters_list, ligands_list)
+    assert expected_cl_dict, observed_cl_dict
 
 
-def test_write_residues():
-    # TODO: implement this
-    assert True
+def test_get_res_dict():
+    """Test correct retrieval of res_dict."""
+    interface_dict = {"int_1": [1, 2, 3], "int_2": [3, 4, 5], "int_3": [27, 28, 29]}
+    cl_dict = {1: ["int_1", "int_2"], 2: ["int_3"]}
+    expected_res_dict = {1: [1, 2, 3, 4, 5], 2: [27, 28, 29]}
+    observed_res_dict = get_residue_dict(cl_dict, interface_dict)
+    assert expected_res_dict == observed_res_dict
+
+
+def test_write_clusters(reference_cl_dict):
+    """Test write_clusters."""
+    cl_filename = "clusters_test.out"
+    write_clusters(reference_cl_dict, cl_filename)
+    expected_content = (
+        f"Cluster 1 -> int_1 int_2{os.linesep}Cluster 2 -> int_3{os.linesep}"
+    )
+    observed_content = open(cl_filename, "r").read()
+    assert expected_content == observed_content
+    os.unlink(cl_filename)
+
+
+def test_write_residues(reference_res_dict):
+    """Test write_residues."""
+    res_filename = "residues_test.out"
+    write_residues(reference_res_dict, res_filename)
+    expected_content = (
+        f"Cluster 1 -> 1 2 3 4 5{os.linesep}Cluster 2 -> 27 28 29{os.linesep}"
+    )
+    observed_content = open(res_filename, "r").read()
+    assert expected_content == observed_content
+    os.unlink(res_filename)
