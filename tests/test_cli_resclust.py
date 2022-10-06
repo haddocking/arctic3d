6fdc09f6d2d35cab5eb69fb695442a22cff077c2
mgiulini <mrcgiulini@gmail.com>
2022-10-06 14:44:52 +0200

    fixed cli tests

--- tests/test_cli_resclust.py
+++ tests/test_cli_resclust.py
@@ -14,11 +14,11 @@ def example_pdbpath():
 
 
 def test_resclust_cli(example_pdbpath):
-    main(example_pdbpath, "100,101,102,133,134,135", None, None, None)
+    main(example_pdbpath, "100,101,102,133,134,135", None, 7.0, "average")
 
 
 def test_wrong_residue_list(example_pdbpath):
     with pytest.raises(SystemExit) as e:
-        main(example_pdbpath, "100,101,102,133,134,135%", None, None, None)
+        main(example_pdbpath, "100,101,102,133,134,135%", None, 9.0, "average")
     assert e.type == SystemExit
     assert e.value.code == 1
