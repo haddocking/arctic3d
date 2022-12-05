f3ac674a4bb91cb78ebae0cfe674d4247cf2f4d8
Marco Giulini <54807167+mgiulini@users.noreply.github.com>
2022-12-05 11:43:34 +0100

    removed useless and commented code (#161)

--- src/arctic3d/modules/example_clustering.py
+++ src/arctic3d/modules/example_clustering.py
@@ -1,12 +1,10 @@
 from clustering import interface_clustering
 
 interface_dict = {
-    "int_1": [16, 18, 19],
-    "int_2": [21, 22, 24, 26, 27, 29, 30, 31, 36, 38, 39, 40],
-    "int_3": [16, 17, 18],
-    "int_4": [20, 24],
-    "int_5": [1, 2, 3],
-    "int_6": [1, 2],
+    "int_1": [],
+    "int_2": [],
+    "int_3": [],
+    "int_4": [],
 }
 
-interface_clustering("./interface_matrix.txt", interface_dict)
+interface_clustering(interface_dict, "../../../tests/golden_data/interface_matrix.txt")
