f7009e29ee4fe02c0daa03f81da453709fabf7bb
mgiulini <mrcgiulini@gmail.com>
2023-01-11 10:35:36 +0100

    corrected test

--- tests/test_cli_resclust.py
+++ tests/test_cli_resclust.py
@@ -14,11 +14,14 @@ def example_pdbpath():
 
 
 def test_resclust_cli(example_pdbpath):
-    main(example_pdbpath, "100,101,102,133,134,135", None, 7.0, "average")
+    main(example_pdbpath, "100,101,102,133,134,135", None, 7.0, "average", "distance")
 
 
 def test_wrong_residue_list(example_pdbpath):
     with pytest.raises(SystemExit) as e:
-        main(example_pdbpath, "100,101,102,133,134,135%", None, 9.0, "average")
+        main(example_pdbpath, "100,101,102,133,134,135%", None, 9.0, "average", "distance")
     assert e.type == SystemExit
     assert e.value.code == 1
+
+def test_resclust_maxclust(example_pdbpath):
+    main(example_pdbpath, "100,101,102,133,134,135", None, 2, "average", "maxclust")
