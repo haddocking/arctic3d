9b296332869ce96470e2dad11bd9663ad70941e9
Marco Giulini <54807167+mgiulini@users.noreply.github.com>
2024-03-23 08:12:05 +0100

    updated blast installation (#394)
    * updated blast installation
    
    * echoed message

--- install_blast_deps.sh
+++ install_blast_deps.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 CWD="$(pwd)"
+echo "Current working directory: ${CWD}"
 DB_DIR=${CWD}/db
 SRC_DIR=${CWD}/src
 OS=$(uname -s)
-
+echo "Operating System: ${OS}"
 echo "Downloading BLAST+ executables and swissprot database..."
 
 #======================================================================#
 echo "Downloading BLAST+..."
 cd "${SRC_DIR}" || exit
 
-if [ "${OS}" == "Darwin" ]; then
+# if the OS is Darwin or Mac OS X, download the Mac OS X version of BLAST+
+# if the OS is Linux, download the Linux version of BLAST+
+if [ "${OS}" == "Darwin" ] || [ "${OS}" == "Mac OS X" ]; then
 	# Do something under Mac OS X platform
-	BLAST_URL="https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-macosx.tar.gz"
+	echo "Downloading BLAST+ for Mac OS X..."
+	BLAST_URL="https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.15.0/ncbi-blast-2.15.0+-x64-macosx.tar.gz"
 elif [ "${OS}" == "Linux" ]; then
 	# Do something under GNU/Linux platform
-	BLAST_URL="https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz"
+	echo "Downloading BLAST+ for Linux..."
+	BLAST_URL="https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.15.0/ncbi-blast-2.15.0+-x64-linux.tar.gz"
 fi
 
 wget "$BLAST_URL" >/dev/null 2>&1
 
-tar -xzf ncbi-blast-2.13.0+-x64-*.tar.gz
-rm ncbi-blast-2.13.0+-x64-*.tar.gz
+tar -xzf ncbi-blast-2.15.0+-x64-*.tar.gz
+rm ncbi-blast-2.15.0+-x64-*.tar.gz
 #======================================================================#
 echo "Downloading SwissProt DB..."
 cd "$DB_DIR" || exit
