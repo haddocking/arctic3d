b109bbd6ca9abbcf6a4aba3b9a25d5e104a8f931
mgiulini <mrcgiulini@gmail.com>
2024-04-04 15:03:47 +0200

    corrected renumber_pdb_from_uniprot test

--- tests/test_pdb.py
+++ tests/test_pdb.py
@@ -281,8 +281,7 @@ def test_pdb_data(inp_pdb_data):
 def test_renumber_pdb_from_uniprot(tricky_pdb):
     """Test renumber_pdb_from_uniprot."""
     renum_pdbf = renumber_pdb_from_uniprot(tricky_pdb, "P00760")
-    exp_resids = [5, 14]
-    exp_resids.extend(list(range(26, 246)))
+    exp_resids = list(range(24, 247))
     renum_pdbf_content = open(renum_pdbf, "r").read().split(os.linesep)
     print(renum_pdbf_content)
     obs_resids = [
