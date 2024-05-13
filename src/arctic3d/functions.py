8176947dbdecdbdae8b5a71106081074d640b5a9
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-05-13 14:07:58 +0200

    add types to `make_request`

--- src/arctic3d/functions.py
+++ src/arctic3d/functions.py
@@ -3,9 +3,11 @@ import time
 import requests
 
 
-def make_request(url, data):
+def make_request(
+    url: str, data: dict[str, list[dict[str, str | int | float | None]]] | None
+) -> dict[str, list[dict[str, str | int | float | None]]] | None:
     """Helper function to make the requests."""
-    for n in range(3):
+    for _ in range(3):
         response = requests.get(url)
         if response.status_code != 404:
             data = response.json()
