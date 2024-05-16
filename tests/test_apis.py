d161f7f86302eb510854756fb8f7459fd4d6ef96
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-05-16 13:04:59 +0200

    remove consistency check from sanity tests

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -1,7 +1,4 @@
 import http
-import json
-from pathlib import Path
-from typing import Callable
 
 import pytest
 import requests
@@ -10,14 +7,12 @@ from arctic3d.cli_localise import UNIPROT_API_URL
 from arctic3d.modules.interface import LIGAND_URL
 from arctic3d.modules.pdb import BESTPDB_URL, PDBE_URL
 
-from . import golden_data
-
 TARGET_UNIPROT = "P07550"
 TARGET_PDB = "1crn"
 
 
 @pytest.mark.sanity
-def test_bestpdb(compare_responses: Callable):
+def test_bestpdb():
     # Make a request to the endpoint and check if the response is 200
     response = requests.get(f"{BESTPDB_URL}/{TARGET_UNIPROT}", timeout=120)
     assert (
@@ -31,15 +26,6 @@ def test_bestpdb(compare_responses: Callable):
 
     # Check if the response is not empty
     assert response.json(), "Response is empty"
-
-    # Check if the response has the same fields as the expected response
-    # Load the expected response
-    with open(Path(golden_data, "bestpdb.json"), "r") as f:
-        expected_response = json.load(f)
-
-    assert compare_responses(
-        response.json(), expected_response
-    ), "Observed response is missing keys, the API might have changed"
 
 
 @pytest.mark.sanity
@@ -74,7 +60,7 @@ def test_pdbe():
 
 
 @pytest.mark.sanity
-def test_uniprot(compare_responses):
+def test_uniprot():
     response = requests.get(f"{UNIPROT_API_URL}/{TARGET_UNIPROT}")
 
     assert (
@@ -88,15 +74,6 @@ def test_uniprot(compare_responses):
 
     # Check if the response is not empty
     assert response.json(), "Response is empty"
-
-    # Check if the response has the same fields as the expected response
-    # Load the expected response
-    with open(Path(golden_data, "uniprot.json"), "r") as f:
-        expected_response = json.load(f)
-
-    assert compare_responses(
-        response.json(), expected_response
-    ), "Observed response is missing keys, the API might have changed"
 
 
 @pytest.mark.sanity
@@ -114,12 +91,3 @@ def test_ligand():
 
     # Check if the response is not empty
     assert response.json(), "Response is empty"
-
-    # Check if the response has the same fields as the expected response
-    # Load the expected response
-    with open(Path(golden_data, "ligand.json"), "r") as f:
-        expected_response = json.load(f)
-
-    assert (
-        response.json() == expected_response
-    ), "Observed response is different from the expected response"
